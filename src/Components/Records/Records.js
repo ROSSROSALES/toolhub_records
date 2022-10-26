dac34384342b4bb3ae4a072f0ef55a87c1599699
ROSSROSALES <ross.erosales@gmail.com>
2022-10-25 21:48:03 -0500

    created 3 required webpages

---
+++ src/Components/Records/Records.js
@@ -0,0 +1,19 @@
+import './Records.css';
+
+function Records() {
+  return (
+    <div className="App">
+          <header className="App-header">
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Records
+            </a>
+          </header>
+        </div>
+  );
+};
+export default Records;
