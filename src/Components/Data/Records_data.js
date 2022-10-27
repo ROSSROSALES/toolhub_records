8ce5e29438cd72db75a73254cfe7061a8dc460bd
Ross Rosales <ross.erosales@gmail.com>
2022-10-26 22:23:48 -0500

    Add Records layout and Data

---
+++ src/Components/Data/Records_data.js
@@ -0,0 +1,10 @@
+// Data pulled from API goes here
+
+
+export const Records_data=
+  {
+    TotalTools: 2703,
+    MissingTools: 2000,
+    PercentMissing: ((2000/2703)*100).toFixed(2)+' %',
+    ToolsEdited: 100,
+  };
