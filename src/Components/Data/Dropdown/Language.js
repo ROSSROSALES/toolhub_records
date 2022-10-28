a3ca24162033986b9d2497858b394fc4fa9595b2
Ross Rosales <ross.erosales@gmail.com>
2022-10-28 00:13:38 -0500

    Update Home/Game Page, css clean up

---
+++ src/Components/Data/Dropdown/Language.js
@@ -0,0 +1,16 @@
+function LanguageDropDown() {
+
+  return (
+    <>
+    <select>
+    <option value="">--Select Language--</option>
+      <option value="English">English</option>
+      <option value="Spanish">Spanish</option>
+      <option value="French">French</option>
+      <option value="Arabic">Arabic</option>
+    </select>
+    </>
+  );
+};
+
+export default LanguageDropDown;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,18 +5,44 @@ function Home() {
   return (
     <>
     <div className="Home">
-      <header className="header">
+      <header className="header-home">
         ToolHub
       </header>
 
       <div class="container">
         <div className="difficulty">
-          <a href="/game" class='button' Style="background-color: #2DD22E"><span>Easy</span></a>
-          <a href="/game" class='button' Style="background-color: #2E2DD2"><span>Medium</span></a>
-          <a href="/game" class='button' Style="background-color: #D22E2D"><span>Hard</span></a>
+          
+          <a href="/game" class='button' Style="background-color: #2DD22E">
+            <span>Easy</span>
+            <div className="subtitle">
+              1 - 4 missing
+            </div>
+            <div className="points">
+              1 point
+            </div>
+          </a>
+          <a href="/game" class='button' Style="background-color: #2E2DD2">
+            <span>Medium</span>
+            <div className="subtitle">
+              5 - 9 missing
+            </div>
+            <div className="points">
+              2 points
+            </div>
+          </a>
+          <a href="/game" class='button' Style="background-color: #D22E2D">
+            <span>Hard</span>
+            <div className="subtitle">
+              > 10 missing
+            </div>
+            <div className="points">
+                4 points
+            </div>
+            
+          </a>
         </div>
       </div>
-      
+
     </div>
     </>
   );
